416fdcdb86e3b470b4eb379d425ff3c7e819e490
jenn-emn <jennifer.mneyra@gmail.com>
2025-12-10 13:23:25 -0300

    03

---
+++ scripts/03.comparison.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+
+
+# SAMPLES
+
+# sample names
+#n1="HG002"
+#n2="HG005"
+n3="HG00733"
+n4="HG01109"
+n5="HG01243"
+n6="HG02055"
+n7="HG02080"
+#n8="HG02109"
+n9="HG02145"
+n10="HG02723"
+n11="HG02818"
+n12="HG03098"
+n13="HG03486"
+n14="HG03492"
+n15="NA18906"
+n16="NA19240"
+n17="NA20129"
+#n18="NA21309"
+names=(${n3} ${n4} ${n5} ${n6} ${n7} ${n9} ${n10} ${n11} ${n12} ${n13} ${n14} ${n15} ${n16} ${n17})
+
+conda activate whatshap_env
+
+for name in "${names[@]}"; do
+
+    to.tsv.pairwise="/home/jennifer/02_datas/04_data_processing_trios/01_intermediate/to_tsv_pairwse.txt"
+    truth.vcf="/home/jennifer/02_datas/04_data_processing_trios/01_intermediate/hprc.mhc/${name}.dip.vcf.gz"
+    phased.vcf="/home/jennifer/02_datas/04_data_processing_trios/01_intermediate/hlamapper.mhc/${name}.mapper.vcf.gz""
+
+    #echo -e "${name}\tchr6\ttruth\twhatshap\t${name}.dip.vcf.gz\t${name}.mapper.vcf.gz" > "${to.tsv.pairwise}"
+
+    # comparison hla.mapper vs trios
+    whatshap compare \
+        --names truth,whatshap \
+        --tsv-pairwise "${to.tsv.pairwise}" \
+        "${truth.vcf}" \
+        "${phased.vcf}"
+
+done
+
+
+
+# end
