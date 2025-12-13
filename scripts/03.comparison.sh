960f21a11e7ce0d58d564510fa651bc5a3dcf280
jenn-emn <jennifer.mneyra@gmail.com>
2025-12-12 23:55:43 -0300

    log correction

--- scripts/03.comparison.sh
+++ scripts/03.comparison.sh
@@ -27,13 +27,16 @@ names=(${n3} ${n4} ${n5} ${n6} ${n7} ${n9} ${n10} ${n11} ${n12} ${n13} ${n14} ${
 
 #conda activate whatshap_env
 
+log="/home/jennifer/02_datas/04_data_processing_trios/01_intermediate/hlamapper.mhc/all.samples.log"
+> "${log}"
+
 for name in "${names[@]}"; do
+
+    echo -e "\nSAMPLE: ${name} ------------------------------------------------" >> "${log}"
 
     tsvpairwise="/home/jennifer/02_datas/04_data_processing_trios/01_intermediate/${name}.tsv.pairwse.txt"
     truthvcf="/home/jennifer/02_datas/04_data_processing_trios/01_intermediate/hprc.mhc/${name}.dip.reheaded.vcf.gz"
     phasedvcf="/home/jennifer/02_datas/04_data_processing_trios/01_intermediate/hlamapper.mhc/${name}.mapper.vcf.gz"
-    log="/home/jennifer/02_datas/04_data_processing_trios/01_intermediate/hlamapper.mhc/${name}.log"
-    > "${log}"
 
     echo -e "${name}\tchr6\ttruth\twhatshap\t${name}.dip.vcf.gz\t${name}.mapper.vcf.gz" > "${tsvpairwise}"
     #> "${tsvpairwise}"
@@ -43,9 +46,9 @@ for name in "${names[@]}"; do
         --names truth,phased \
         --tsv-pairwise "${tsvpairwise}" \
         "${truthvcf}" \
-        "${phasedvcf}"
+        "${phasedvcf}" &>> "${log}"
 
-done &>> "${log}" # o standard output is going to the .log
+done
 
 
 
