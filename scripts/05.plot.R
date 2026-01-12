76819acf1262774b580880b81e2c8a581d7bdd72
jenn-emn <jennifer.mneyra@gmail.com>
2026-01-12 14:57:09 -0300

    cleaning

--- scripts/05.plot.R
+++ scripts/05.plot.R
@@ -44,27 +44,29 @@ for (i in seq(1,length(names))) {
     y_pos <- 1 
     p <- ggplot(data_clean) +
         geom_segment(aes(x = min(pos_start) - 100000, 
-                        xend = max(pos_end) + 100000, 
-                        y = y_pos, yend = y_pos), 
+                    xend = max(pos_end) + 100000, 
+                    y = y_pos, yend = y_pos), 
                     color = "gray80", size = 2) +
         geom_rect(aes(xmin = pos_start - 500, xmax = pos_end + 500, 
-                        ymin = y_pos - 0.1, ymax = y_pos + 0.1), 
+                    ymin = y_pos - 0.1, ymax = y_pos + 0.1), 
                     fill = "red", color = "darkred") +
         geom_text(aes(x = (pos_start + pos_end)/2, 
-                        y = y_pos + 0.3, 
-                        label = paste0(block_id, ":", block_size)), 
+                    y = y_pos + 0.3, 
+                    label = paste0(block_id, ":", block_size)), 
                     size = 3, vjust = 0, angle = 45) +
-        scale_x_continuous(labels = scales::comma_format(), name = "Posição no Chr6 (bp)") +
-        scale_y_continuous(limits = c(0.5, 2), name = "Block: switches") +
+        scale_x_continuous(labels = scales::comma_format(),
+                    name = "Posição no Chr6 (bp)") +
+        scale_y_continuous(limits = c(0.5, 2),
+                    name = "Block: switches") +
         labs(title = paste0("Localization of switched blocks in the Chromosome 6 (",names[i],")"),
-            #subtitle = names[i],
-            #caption = "Note"
-            ) +
+                    #subtitle = names[i],
+                    #caption = "Note"
+                    ) +
         theme_minimal() +
         theme(axis.text.y = element_blank(),
-                axis.ticks.y = element_blank(),
-                panel.grid.major.y = element_blank(),
-                panel.grid.minor.y = element_blank())
+                    axis.ticks.y = element_blank(),
+                    panel.grid.major.y = element_blank(),
+                    panel.grid.minor.y = element_blank())
 
     ggsave(path_plot, width = 12, height = 4, dpi = 300)
     
